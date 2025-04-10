5675ee0efdfa6b54485973552e3069574d7523a8
Ethan <ethanlahr334@gmail.com>
2025-04-10 10:37:40 -0400

    Cleaned up previous commit for neatness

--- client-server-client/client.py
+++ client-server-client/client.py
@@ -16,18 +16,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data = {}
         data["username"] = username
         task = input("Would you like to view your inbox (vi), or send a message (sm): ")
+        data["task"] = task
         if task == "vi":
-            data["task"] = "vi"
-            json_data = json.dumps(data).encode()
-            send_data(json_data)
+            pass
         elif task == "sm":
             to_user = input("Who do you want to send a message to? ")
             message = input("What is the message? ")
-            data["task"] = "sm"
             data["to_user"] = to_user
             data["message"] = message
-            json_data = json.dumps(data).encode()
-            send_data(json_data)
         else:
             print("task invalid")
             task = None
+            continue
+
+        json_data = json.dumps(data).encode()
+        send_data(json_data)
